7df49e769887eccfd72222783557752bae63e7f7
yansaid21 <yansaid21@gmail.com>
2022-03-20 12:23:22 -0500

    crea espacio nodos,no es lo esperado

--- read_data.py
+++ read_data.py
@@ -16,22 +16,25 @@ class ReadData:
                 #for line in self.file:
                 print(file_line, end='')
                 #Crear el incremento en la linea antes de salir while
+                #return file_line
                 file_line = data_file.readline()
-                return file_line
+                
         data_file.close()
                 
     def count_Lines():
+        counter=0;
         with io.open("Nodos_prueba.txt",'r+', encoding='utf-8') as data_file:
             file_line = data_file.readline()
-            counter=0;
+            
             while(file_line != ''):
                 #for line in self.file:
-                counter+1
+                counter=counter+1
                 #Crear el incremento en la linea antes de salir while
                 file_line = data_file.readline()
                 print(counter)
-                return counter
+               
         data_file.close()
+        return counter
     #readlines nos devuelve una lista
     def show_file_content_v2(self):
         with io.open('Nodos_prueba.txt','r+', encoding='utf-8') as data_file:
